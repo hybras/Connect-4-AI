c5a76b28ad9ca122aac0c8d37d585f6fa39eb706
hybras <24651269+hybras@users.noreply.github.com>
2020-06-28 12:58:52 -0400

    Hello async world example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
+use std::time::Duration;
+use tokio::task;
+
+#[tokio::main]
+async fn main() {
+    let task = task::spawn(hello());
+    println!("Going to hello");
+    match task.await {
+        Ok(_) => {
+            println!("You've been helloed");
+        }
+        Err(_) => println!("Hello failed"),
+    }
+}
+
+async fn hello() {
+    tokio::time::delay_for(Duration::from_secs(6)).await;
     println!("Hello, world!");
 }
