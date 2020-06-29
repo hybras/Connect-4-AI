193c2d36d4d051f0d8e1e734a8104c8701188f6a
hybras <24651269+hybras@users.noreply.github.com>
2020-06-29 11:05:12 -0400

    Move main method to a test

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,5 @@
 use crate::state::{Board, Piece};
 
-fn main() {
-	let mut board: Board = Default::default();
-	board.make_move(Piece::Blue, 0).unwrap();
-	board.make_move(Piece::Red, 1).unwrap();
-	println!("board:\n{}", board);
-}
+fn main() {}
 
 mod state;
