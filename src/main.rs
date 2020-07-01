399e89c318e5eafaa5e9d1933f382492cb67b205
hybras <24651269+hybras@users.noreply.github.com>
2020-06-30 21:01:30 -0400

    Add playthrough to main func

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,56 @@
 use crate::state::{Board, Piece};
+use std::io::stdin;
+fn main() -> Result<(), std::io::Error> {
+	let mut board = Board::default();
+	let winner = main_loop(&mut board);
+	game_end_message(winner);
+	Ok(())
+}
 
-fn main() {}
+fn main_loop(board: &mut Board) -> Piece {
+	let stdin = stdin();
+
+	let mut is_blue_turn = true;
+
+	let winner;
+	loop {
+		if is_blue_turn {
+			println!("It's blue's turn!");
+		} else {
+			println!("It's red's turn!");
+		}
+		println!("{}", board);
+		print!("Your move: ");
+		let mut col = String::new();
+		stdin.read_line(&mut col).unwrap();
+		println!();
+		let col = col.trim().parse::<i8>().unwrap();
+		board
+			.make_move(
+				if is_blue_turn {
+					Piece::Blue
+				} else {
+					Piece::Red
+				},
+				col as usize,
+			)
+			.unwrap();
+		if let Some(won) = board.get_winner() {
+			winner = won;
+			break;
+		} else {
+			is_blue_turn = !is_blue_turn;
+		}
+	}
+	winner
+}
+
+fn game_end_message(winner: Piece) {
+	match winner {
+		Piece::Red => println!("Red won"),
+		Piece::Blue => println!("Blue won"),
+		Piece::Empty => println!("It was a tie"),
+	}
+}
 
 mod state;
