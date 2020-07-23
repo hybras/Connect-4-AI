dac35f1b04ebb5a6e69dc43d4bda56f67b6d80f8
hybras <24651269+hybras@users.noreply.github.com>
2020-07-23 11:54:13 -0400

    Basic Board impl for Bitboard

--- src/state/bit_board.rs
+++ src/state/bit_board.rs
@@ -7,3 +7,33 @@ struct BitBoard {
 	height: usize,
 	width: usize,
 }
+
+impl Board for BitBoard {
+	fn new(width: usize, height: usize) -> Self {
+		todo!()
+	}
+	fn width(&self) -> usize {
+		self.width
+	}
+	fn height(&self) -> usize {
+		self.height
+	}
+	fn find_height(&self, col: &usize) -> usize {
+		todo!()
+	}
+	fn num_moves(&self) -> usize {
+		todo!()
+	}
+	fn get_winner(&self) -> Option<Option<Piece>> {
+		todo!()
+	}
+	fn make_move(&mut self, col: &usize) -> Result<(), String> {
+		todo!()
+	}
+}
+
+impl Default for BitBoard {
+	fn default() -> Self {
+		Self::new(7, 6)
+	}
+}
